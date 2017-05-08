a4fe4d3ec3c71eb0149382f2221263a668a71079
songhj <songhj@SONGHJ-PC>
2017-05-08 14:19:01 +0900

    commit

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -7,6 +7,9 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using Bot_Application1.Lib;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace Bot_Application1
 {
@@ -17,17 +20,118 @@ namespace Bot_Application1
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
+        /// 
+
+        struct Card
+        {
+            string sid;
+            string carTitle;
+
+            public Card(string sid, string carTitle)
+            {
+                this.sid = sid;
+                this.carTitle = carTitle;
+            }
+            //string carImage;
+            //string carButton;
+            //string carButtonContent;
+        }
+
+        List<Card> card = null;
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
+            string strConn = "Data Source=faxtimedb.database.windows.net;Initial Catalog=taihoML;User ID=faxtime;Password=test2016!;";
+
+            card = new List<Card>();
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
                 // calculate something for us to return
                 int length = (activity.Text ?? string.Empty).Length;
 
                 // return our reply to the user
                 Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
                 await connector.Conversations.ReplyToActivityAsync(reply);
+
+                // Db
+                DbConnect db = new DbConnect();
+                List<Car> car = db.SelectDb();
+
+                // HeroCard
+                Activity replyToConversation = activity.CreateReply("Should go to conversation, with a hero card");
+
+                replyToConversation.Recipient = activity.From;
+                replyToConversation.Type = "message";
+                replyToConversation.Attachments = new List<Attachment>();
+                replyToConversation.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+
+                List<CardImage>[] cardImages = new List<CardImage>[car.Count];
+                List<CardAction>[] cardButtons = new List<CardAction>[car.Count];
+                CardAction[] plButton = new CardAction[car.Count];
+                HeroCard[] plCard = new HeroCard[car.Count];
+                Attachment[] plAttachment = new Attachment[car.Count];
+
+                for (int i = 0; i < car.Count; i++)
+                {
+                    if(car[i].erase == "F")
+                    {
+                        cardImages[i] = new List<CardImage>();
+                        cardImages[i].Add(new CardImage(url: car[i].carImage));
+
+                        cardButtons[i] = new List<CardAction>();                        
+                        plButton[i] = new CardAction()
+                        {
+                            Value = car[i].carButtonContent,
+                            Type = "imBack",
+                            Title = car[i].carButton
+                        };
+                        cardButtons[i].Add(plButton[i]);
+
+                        plCard[i] = new HeroCard()
+                        {
+                            Title = car[i].carTitle,
+                            Images = cardImages[i],
+                            Buttons = cardButtons[i]
+                        };
+
+                        plAttachment[i] = plCard[i].ToAttachment();
+                        replyToConversation.Attachments.Add(plAttachment[i]);
+                    }
+                }
+
+                /*
+                List<CardImage> cardImages = new List<CardImage>();
+                cardImages.Add(new CardImage(url: "http://webbot02.azurewebsites.net/hyundai/images/price/Grandeur_24spec.PNG"));
+                cardImages.Add(new CardImage(url: "http://webbot02.azurewebsites.net/hyundai/images/price/Grandeur_30spec.PNG"));
+
+                List<CardAction> cardButtons = new List<CardAction>();
+
+                CardAction plButton = new CardAction()
+                {
+                    Value = "car1",
+                    Type = "imBack",
+                    Title = "WikiPedia Page"
+                };
+                cardButtons.Add(plButton);
+
+                HeroCard plCard = new HeroCard()
+                {
+                    Title = "I'm a hero card1",
+                    Subtitle = "Pig Latin Wikipedia Page",
+                    Images = cardImages,
+                    Buttons = cardButtons
+                };
+
+                Attachment plAttachment = plCard.ToAttachment();
+                Attachment plAttachment2 = plCard.ToAttachment();
+
+                replyToConversation.Attachments.Add(plAttachment);
+                replyToConversation.Attachments.Add(plAttachment2);
+                */
+
+                var reply1 = await connector.Conversations.SendToConversationAsync(replyToConversation);
+
             }
             else
             {
---
+++ Lib/DbConnect.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Text;
+using System.Collections;
+using System.Data;
+
+namespace Bot_Application1.Lib
+{
+    public class Car
+    {
+        public string sid;
+        public string carTitle;
+        public string carImage;
+        public string carButton;
+        public string carButtonContent;
+        public string erase;
+    }
+
+    public class DbConnect
+    {
+        string strConn = "Data Source=faxtimedb.database.windows.net;Initial Catalog=taihoML;User ID=faxtime;Password=test2016!;";
+        StringBuilder sb = new StringBuilder();
+
+        public void ConnectDb()
+        {
+            SqlConnection conn = null;
+
+            try
+            {
+                conn = new SqlConnection(strConn);
+                conn.Open();
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+
+        }
+
+        public List<Car> SelectDb()
+        {
+            SqlDataReader rdr = null;
+            List<Car> card = new List<Car>();
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from tbl_card_test";
+
+                rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (rdr.Read())
+                {
+                    string sid = rdr["SID"].ToString();
+                    string carTitle = rdr["CAR_TITLE"] as string;
+                    string carImage = rdr["CAR_IMAGE"] as string;
+                    string carButton = rdr["CAR_BUTTON"] as string;
+                    string carButtonContent = rdr["CAR_BUTTON_CONTENT"] as string;
+                    string erase = rdr["ERASE"] as string;
+
+                    Car car = new Car();
+                    car.sid = sid;
+                    car.carTitle = carTitle;
+                    car.carImage = carImage;
+                    car.carButton = carButton;
+                    car.carButtonContent = carButtonContent;
+                    car.erase = erase;
+
+                    card.Add(car);
+                }
+            }
+
+            return card;
+        }
+
+    }
+}
